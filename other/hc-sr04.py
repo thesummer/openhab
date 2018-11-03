374d7e089accee4c3875e1489aa101f59ccd4c18
Jan Sommer <soja-lists@aries.uberspace.de>
2018-11-03 18:21:26 +0100

    Finish script for water level

--- other/hc-sr04.py
+++ other/hc-sr04.py
@@ -21,7 +21,6 @@ def cbf(gpio, level, tick):
     global callBackEvent
     global startTime
     global stopTime
-    print(gpio, level, tick)
     if level == 1: # Rising edge
         startTime = tick
     elif level == 0:
@@ -38,22 +37,28 @@ def main():
     pi.set_mode(20, pigpio.OUTPUT)
     pi.set_mode(21, pigpio.INPUT)
     cb = pi.callback(21, pigpio.EITHER_EDGE, cbf)
-    
-    callBackEvent.clear()
-    pi.write(20, 1)
-    time.sleep(0.00001)
-    pi.write(20, 0)
-    
-    if callBackEvent.wait(0.5):
-        water_level = abs(startTime - stopTime) / 10000.0 * 343.5
-        print(water_level)
-        #v = 'water_level value=%s' % water_level
-        #r = requests.post("http://%s:8086/write?db=%s" %(IP, DB), auth=(USER, PASSWORD), data=v)
-        #if r.status_code != 204:
-        #print '[ERROR] Failed to add temperature to influxdb (%d)' %r.status_code
-        
-    
-        #time.sleep(5*60)
+
+    while True:
+        distance = 0.0
+        for i in  range(10):
+            callBackEvent.clear()
+            pi.write(20, 1)
+            time.sleep(0.00001)
+            pi.write(20, 0)
+            if callBackEvent.wait(0.5):
+                distance += abs(startTime - stopTime) / 10000.0 * 343.5 / 2.0
+                time.sleep(0.5)
+            else:
+                print("Timeout")
+        distance = distance / 10.0
+        # Set 0% to 2.5 cm and 100% to 6 cm
+        water_level = -200.0/7*distance + 1200.0/7
+        v = 'water_level value=%s' % water_level
+        r = requests.post("http://%s:8086/write?db=%s" %(IP, DB), auth=(USER, PASSWORD), data=v)
+        if r.status_code != 204:
+            print '[ERROR] Failed to add water level to influxdb (%d)' %r.status_code
+
+        time.sleep(5*60)
  
   
 if __name__=="__main__":
